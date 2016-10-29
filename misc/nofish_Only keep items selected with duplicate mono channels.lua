51a823570eef2dd1ad1161029a0d9005203b9149
nofish <nofishonfriday@googlemail.com>
2016-10-29 14:57:52 +0200

    display l / r samples

--- misc/nofish_Only keep items selected with duplicate mono channels.lua
+++ misc/nofish_Only keep items selected with duplicate mono channels.lua
@@ -63,6 +63,17 @@ debug = 1
        end %if
        --]]
        
+    --[[
+    rescript example:
+    tr = RPR_GetTrack(0, 0)
+    aa = RPR_CreateTrackAudioAccessor(tr)
+    buf = list([0]*2*1024) # 2 channels, 1024 samples each, initialized to zero
+    pos = 0.0
+    (ret, buf) = GetAudioAccessorSamples(aa, 44100, 2, pos, 1024, buf)
+    # buf now holds the first 2*1024 audio samples from the track.
+    # typically GetAudioAccessorSamples() would be called within a loop, increasing pos each time.
+    --]]
+       
     if item then
       local item_pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
       local take = reaper.GetActiveTake(item) -- Get the active take   
@@ -74,13 +85,74 @@ debug = 1
       -- Create take audio accessor
       local aa = reaper.CreateTakeAudioAccessor(take)
       -- Get the start time of the audio that can be returned from this accessor
-        local aa_start = reaper.GetAudioAccessorStartTime(aa)
-        -- Get the end time of the audio that can be returned from this accessor
-        local aa_end = reaper.GetAudioAccessorEndTime(aa)
-        local take_source_len, length_is_QN = reaper.GetMediaSourceLength(take_pcm_source)
-        -- Get the number of channels in the source media.
-         local take_source_num_channels = reaper.GetMediaSourceNumChannels(take_pcm_source)
-         msg(take_source_num_channels)
+      local aa_start = reaper.GetAudioAccessorStartTime(aa)
+      -- Get the end time of the audio that can be returned from this accessor
+      local aa_end = reaper.GetAudioAccessorEndTime(aa)
+      local take_source_len, length_is_QN = reaper.GetMediaSourceLength(take_pcm_source)
+      -- Get the number of channels in the source media.
+      local take_source_num_channels = reaper.GetMediaSourceNumChannels(take_pcm_source)
+      -- msg(take_source_num_channels)
+      local take_source_sample_rate = reaper.GetMediaSourceSampleRate(take_pcm_source)
+      -- How many samples are taken from audio accessor and put in the buffer
+      local samples_per_channel = take_source_sample_rate
+      -- msg(samples_per_channel)
+      -- Samples are collected to this buffer
+      local buffer = reaper.new_array(samples_per_channel * take_source_num_channels)
+      
+      local total_samples = math.ceil((aa_end - aa_start) * take_source_sample_rate)
+      local block = 0
+      local sample_count = 0
+      local audio_end_reached = false
+      local offs = aa_start
+      
+      local abs = math.abs
+      
+      
+      -- *** Loop through samples *** ---
+        while sample_count < total_samples do
+          if audio_end_reached then
+            break
+          end
+      
+          -- Get a block of samples from the audio accessor.
+          -- Samples are extracted immediately pre-FX,
+          -- and returned interleaved (first sample of first channel,
+          -- first sample of second channel...). Returns 0 if no audio, 1 if audio, -1 on error.
+          local aa_ret =
+                  reaper.GetAudioAccessorSamples(
+                                                  aa,                       -- AudioAccessor accessor
+                                                  take_source_sample_rate,  -- integer samplerate
+                                                  take_source_num_channels, -- integer numchannels
+                                                  offs,                     -- number starttime_sec
+                                                  samples_per_channel,      -- integer numsamplesperchannel
+                                                  buffer                    -- reaper.array samplebuffer
+                                                )
+          -- msg(buffer)
+          if aa_ret <= 0 then
+            --msg("no audio or other error")
+            --return
+          end
+          -- msg(aa_ret)
+      
+          for i=1, #buffer, take_source_num_channels do
+            if sample_count == total_samples then
+              audio_end_reached = true
+              break
+            end
+            for j=1, take_source_num_channels do
+              local buf_pos = i+j-1
+              -- local curr_val = abs(buffer[buf_pos])
+              local curr_val = (buffer[buf_pos])
+              msg(curr_val)
+            end
+            msg("===")
+            sample_count = sample_count + 1
+          end
+          block = block + 1
+          offs = offs + samples_per_channel / take_source_sample_rate -- new offset in take source (seconds)
+        end
+      
+        reaper.DestroyAudioAccessor(aa)
     end
         
         
