067e1ee3ab5f747e7ad96eff35e774e2c76e805e
nofish <nofishonfriday@googlemail.com>
2016-10-31 11:39:18 +0100

    0.1 test version works on comparing samples

--- misc/nofish_Only keep items selected with duplicate mono channels.lua
+++ misc/nofish_Only keep items selected with duplicate mono channels.lua
@@ -1,15 +1,27 @@
 --[[
 nofish_Only keep items selected with duplicate mono channels.lua
-v0.9
+v0.1
 
-works on selected stereo (2-channel) items (1- or more-channel items are ignored) 
-checks if left and right channel are 100% identical
+works on selected stereo (2-channel) items (selected 1- or more than 2-channels items are ignored in the test)
+checks if left and right channel are identical (= double mono items)
+and if so it keeps only these items selected
 --]]
 
 
+--[[
+set rounding precision here...
+not used currently
+--]]
+decimals = 4
 
+
+function trunc(num, digits)
+  local mult = 10^(digits)
+  return math.modf(num*mult)/mult
+end
 
 -- DBG
+reaper.ShowConsoleMsg("")
 function msg(m)
   if debug == 1 then reaper.ShowConsoleMsg(tostring(m) .. "\n")
   end
@@ -20,7 +32,7 @@ tablePos = 1
 
 
 -- set item selection from selItemsTable
-function RestoreSelectedItems (table)
+function selectDoubleMonoItems (table)
   reaper.Main_OnCommand(40289, 0) -- unsel all items
   for _, item in ipairs(table) do
     reaper.SetMediaItemSelected(item, true)
@@ -29,37 +41,20 @@ end
 
 
 
-
-
 function main()
 debug = 1
- 
-
   reaper.Undo_BeginBlock() 
-  -- LOOP THROUGH SELECTED ITEMS
-  
+ 
   selected_items_count = reaper.CountSelectedMediaItems(0)
+  msg(selected_items_count .. " items selected")
   
-  -- INITIALIZE loop through selected items
+  -- loop through selected items
   for i = 0, selected_items_count-1  do
     -- GET ITEMS
     item = reaper.GetSelectedMediaItem(0, i) -- Get selected item i
     itemIsStereo = false
-        
-    --[[
-    Audio accessor rescript example:
-    tr = RPR_GetTrack(0, 0)
-    aa = RPR_CreateTrackAudioAccessor(tr)
-    buf = list([0]*2*1024) # 2 channels, 1024 samples each, initialized to zero
-    pos = 0.0
-    (ret, buf) = GetAudioAccessorSamples(aa, 44100, 2, pos, 1024, buf)
-    # buf now holds the first 2*1024 audio samples from the track.
-    # typically GetAudioAccessorSamples() would be called within a loop, increasing pos each time.
-    --]]
-       
+           
     if item then
-      
-      -- comment for prod
       local progressDisplay = "processing item " .. (i+1)
       msg(progressDisplay)
       
@@ -79,14 +74,12 @@ debug = 1
       local take_source_num_channels = reaper.GetMediaSourceNumChannels(take_pcm_source)
      
      
-      -- msg(selected_items_count)
-      -- msg(take_source_num_channels) 
       if (take_source_num_channels == 2) then
       
       local take_source_sample_rate = reaper.GetMediaSourceSampleRate(take_pcm_source)
            -- How many samples are taken from audio accessor and put in the buffer
            local samples_per_channel = take_source_sample_rate
-           -- msg(samples_per_channel)
+    
            -- Samples are collected to this buffer
            local buffer = reaper.new_array(samples_per_channel * take_source_num_channels)
            
@@ -100,6 +93,8 @@ debug = 1
            
            
            -- *** Loop through samples *** ---
+           -- Audio accessor code bits by spk77, thanks
+           
            
              while sample_count < total_samples do
                if audio_end_reached then
@@ -110,15 +105,27 @@ debug = 1
                -- Samples are extracted immediately pre-FX,
                -- and returned interleaved (first sample of first channel,
                -- first sample of second channel...). Returns 0 if no audio, 1 if audio, -1 on error.
-               local aa_ret =
-                       reaper.GetAudioAccessorSamples(
-                                                       aa,                       -- AudioAccessor accessor
-                                                       take_source_sample_rate,  -- integer samplerate
-                                                       take_source_num_channels, -- integer numchannels
-                                                       offs,                     -- number starttime_sec
-                                                       samples_per_channel,      -- integer numsamplesperchannel
-                                                       buffer                    -- reaper.array samplebuffer
-                                                     )
+               
+               --[[
+               Audio accessor rescript example:
+               tr = RPR_GetTrack(0, 0)
+               aa = RPR_CreateTrackAudioAccessor(tr)
+               buf = list([0]*2*1024) # 2 channels, 1024 samples each, initialized to zero
+               pos = 0.0
+               (ret, buf) = GetAudioAccessorSamples(aa, 44100, 2, pos, 1024, buf)
+               # buf now holds the first 2*1024 audio samples from the track.
+               # typically GetAudioAccessorSamples() would be called within a loop, increasing pos each time.
+               --]]
+               
+               local aa_ret = reaper.GetAudioAccessorSamples(
+                 aa,                       -- AudioAccessor accessor
+                 take_source_sample_rate,  -- integer samplerate
+                 take_source_num_channels, -- integer numchannels
+                 offs,                     -- number starttime_sec
+                 samples_per_channel,      -- integer numsamplesperchannel
+                 buffer                    -- reaper.array samplebuffer
+                )
+                
                -- msg(buffer)
                if aa_ret <= 0 then
                  --msg("no audio or other error")
@@ -142,21 +149,28 @@ debug = 1
                    msg(curr_val)
                  end
                  --]]
+                 
                  local buf_pos_left = i -- left channel
                  local curr_val_left = (buffer[buf_pos_left])
+                 -- truncate to x decimals
+                 trunc_val_left = trunc(curr_val_left, decimals)
+                 
                  local buf_pos_right = i+1 -- right channel, interleaved
                  local curr_val_right = (buffer[buf_pos_right])
-                 
+                 -- truncate
+                 trunc_val_right = trunc(curr_val_right, decimals)
+              
                  if (curr_val_left ~= curr_val_right) then
-                  -- uncomment here if want to see sample analysis (warning: big data output)
-                  --[[
+                 
+                  -- uncomment here if want to see sample analysis (warning: huge data output)
+                  
                   msg("is stereo: ")
-                  msg(curr_val_left)
-                  msg(curr_val_right)
+                  reaper.ShowConsoleMsg(curr_val_left)
+                  reaper.ShowConsoleMsg("\n")
+                  reaper.ShowConsoleMsg(curr_val_right)
+                  reaper.ShowConsoleMsg("\n")
                   msg("===")
-                  --]]
                 
-                  -- reaper.SetMediaItemInfo_Value(item, "B_UISEL", 0)
                   itemIsStereo = true
                   break
                   end 
@@ -168,8 +182,9 @@ debug = 1
              if (itemIsStereo == false) then -- 2-channel item is double-mono
               selItemsTable[tablePos] = item -- store this item in item selection table for later
               tablePos = tablePos+1
+              msg("double mono item detected")
             end
-             reaper.DestroyAudioAccessor(aa)
+          reaper.DestroyAudioAccessor(aa)
       end -- if (take_source_num_channels == 2)
       -- msg("next item")
     end -- if item then
@@ -180,6 +195,7 @@ end -- main()
 
 main()
 
-RestoreSelectedItems(selItemsTable)
-reaper.UpdateArrange() -- Update the arrangement (often needed)
--- reaper.ShowMessageBox("Script executed in (s): "..tostring(reaper.time_precise() - time_os), "", 0)
+selectDoubleMonoItems(selItemsTable)
+reaper.UpdateArrange()
+msg("Done !")
+
