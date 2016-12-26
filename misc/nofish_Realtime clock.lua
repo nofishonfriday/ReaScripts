79a79ca7cc5fd9310f645c8c1427221d419a227d
nofish <nofishonfriday@googlemail.com>
2016-12-26 20:02:29 +0100

    blah

--- misc/nofish_Realtime clock.lua
+++ misc/nofish_Realtime clock.lua
@@ -2,7 +2,7 @@
  * ReaScript Name: nofish_Realtime clock
  * Version: 1.0
  * Author: nofish
-  * About:
+ * About:
  *   displays a small clock window
      see http://forum.cockos.com/showthread.php?t=165884
 --]]
