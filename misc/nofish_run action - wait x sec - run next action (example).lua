6eb01ea89afe161677231b85c645cccb4f072cb0
nofish <nofishonfriday@googlemail.com>
2017-03-25 15:45:41 +0100

    added ReaPack Header

--- misc/nofish_run action - wait x sec - run next action (example).lua
+++ misc/nofish_run action - wait x sec - run next action (example).lua
@@ -1,15 +1,42 @@
---- user area ---
+--[[
+ * ReaScript Name: nofish_run action - wait x sec - run next action (example)
+ * Version: 1.0
+ * Author: nofish
+ * About:
+ *  example script for waiting x seconds between running two actions  
+ *  see http://forum.cockos.com/showthread.php?t=189856
+--]]
+
+--[[
+ Changelog:
+ * v1.0
+    + Initial release
+--]]
+
+--- reaper.ShowConsoleMsg("") -- clear console
+
+
+----------------------------------------------------
+-- EDIT THE WAITING TIME IN SECONDS BETWEEN RUNNING ACTIONS HERE
+
 wait_time_in_seconds = 5
---- end of user area ---
+
+----------------------------------------------------
 
 
-reaper.ShowConsoleMsg("") -- clear console
+
+----------------------------------------------------
+-- PUT ACTION(S) YOU WANT TO RUN IMMEDIATELY HERE
 
 reaper.Main_OnCommand(1007, 0) -- Transport: Play
 
+----------------------------------------------------
+
+
+-- waiting code taken from schwa, thanks
+-- http://forum.cockos.com/showthread.php?t=168270
 lasttime=os.time()
 loopcount=0
-
 
 function runloop()
   local newtime=os.time()
@@ -17,30 +44,24 @@ function runloop()
   if (loopcount < 1) then
     if newtime-lasttime >= wait_time_in_seconds then
       lasttime=newtime
-      -- do whatever you want to do every 10 seconds
-      reaper.ShowConsoleMsg("waited 2 seconds\n...")
+      -- do whatever you want to do every x seconds
+      -- reaper.ShowConsoleMsg("waited ".. wait_time_in_seconds .. " seconds")
       loopcount = loopcount+1
     end
   else
+    ----------------------------------------------------
+    -- PUT ACTION(S) YOU WANT TO RUN AFTER WAITING HERE
+    
     reaper.Main_OnCommand(1016, 0) -- Transport: Stop
     
-    reaper.ShowConsoleMsg("stop !")
+    ----------------------------------------------------
+    
+    -- reaper.ShowConsoleMsg("stop !")
     loopcount = loopcount+1
-    
-    -- is_new,name,sec,cmd,rel,res,val = reaper.get_action_context();  
-    -- reaper.ShowConsoleMsg(cmd)
-    -- reaper.ShowConsoleMsg("\n")
-    
-    -- gfx.quit()
   end
-    
-  
- 
-
-  -- note, this program will run forever, until terminated by the action
-  -- "close all running reascripts", which is possibly not ideal.
-  if (loopcount < 2) then reaper.defer(runloop) end
-
+  if 
+    (loopcount < 2) then reaper.defer(runloop) 
+  end
 end
 
 reaper.defer(runloop)
