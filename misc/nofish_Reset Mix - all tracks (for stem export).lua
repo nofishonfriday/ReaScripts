c8db8931787593b5679c718eba9e7199f4252208
nofish <nofishonfriday@googlemail.com>
2017-10-21 15:24:18 +0200

    add 'Reset Mix - all tracks' + upd idx

---
+++ misc/nofish_Reset Mix - all tracks (for stem export).lua
@@ -0,0 +1,49 @@
+--[[
+ * ReaScript Name: nofish_Reset Mix - all tracks (for stem export)
+ * Version: 1.0
+ * Author: nofish
+ * About:
+ *  Does the following:
+ *  - Bypass all automation
+ *  - Bypass all track FX (except VSTi)
+ *  - Reset all track volume, reset all track pan
+ *  - Reset master volume, Bypass master FX
+ *  - Optionally: Reset all item and take volume
+--]]
+
+--[[
+ Changelog:
+ * v1.0 - October 21 2017
+    + Initial release
+--]]
+
+
+reaper.PreventUIRefresh(1)
+reaper.Undo_BeginBlock()
+
+
+reaper.Main_OnCommand(40885, 0) -- Global automation override: Bypass all automation
+reaper.Main_OnCommand(40296, 0) -- Track: Select all tracks
+
+reaper.Main_OnCommand(reaper.NamedCommandLookup("_NF_BYPASS_FX_EXCEPT_VSTI_FOR_SEL_TRACKS"), 0) -- Bypass FX (except VSTi) for sel. tracks
+reaper.Main_OnCommand(reaper.NamedCommandLookup("_XENAKIOS_RESETTRACKVOLANDPAN1"), 0) -- Reset volume and pan of selected tracks
+
+reaper.Main_OnCommand(40297, 0) -- Unselect all tracks
+
+reaper.Main_OnCommand(reaper.NamedCommandLookup("_XENAKIOS_SETMASTVOLTO0"), 0) -- Set master volume to 0 dB
+reaper.Main_OnCommand(reaper.NamedCommandLookup("_SWS_DISMASTERFX"), 0) -- Disable master FX
+
+--[[
+-- optionally reset item and take volume
+reaper.Main_OnCommand(40182, 0) -- Select all items
+reaper.Main_OnCommand(reaper.NamedCommandLookup("_XENAKIOS_RESETITEMVOL"), 0) -- Reset item volume to 0.0 dB
+reaper.Main_OnCommand(reaper.NamedCommandLookup("_XENAKIOS_RESETTAKEVOL"), 0) -- Reset active take volume to 0.0 dB
+reaper.Main_OnCommand(40289, 0) -- Unselect all items
+--]]
+
+
+reaper.PreventUIRefresh(-1)
+reaper.Undo_EndBlock("Script: nofish_Reset Mix", -1)
+
+
+
