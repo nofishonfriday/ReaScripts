5f17029121369b2941f012405eb963c56c473794
nofish <nofishonfriday@googlemail.com>
2018-10-30 11:41:53 +0100

    "Mute first n HW outs..." - 1.01: add option to disable input monitoring during pre-roll

--- misc/Mute first n hardware outs on tracks that are currently recording.lua
+++ misc/Mute first n hardware outs on tracks that are currently recording.lua
@@ -1,6 +1,6 @@
 --[[
  * ReaScript Name: Mute first n hardware outs on tracks that are currently recording
- * Version: 1.0
+ * Version: 1.01
  * Author: nofish
  * Author URI: https://forum.cockos.com/member.php?u=6870  
  * Donation: https://paypal.me/nofish
@@ -23,12 +23,15 @@
  * v1.0 (October 10 2018)
   + Initial Release
   + Script can be assigned to a toolbar button (lights when active), press again to exit
+ * v1.01 (October 30 2018)
+  + Add option to disable input monitoring during pre-roll
 --]]
 
 
 -- USER CONFIG AREA -----------------------------------------------------------
 
 NUM_HW_OUTS_TO_MUTE = 1 -- number of first hardware outs to mute
+DISABLE_INPUT_MONITORING_DURING_PRE_ROLL = true
 
 ------------------------------------------------------- END OF USER CONFIG AREA
 
@@ -41,7 +44,6 @@ function Msg(m)
     return reaper.ShowConsoleMsg(tostring(m) .. "\n")
   end
 end
-
 
 --[[
 function PromptUser()
@@ -59,11 +61,9 @@ function PromptUser()
 end
 --]]
 
-
 -- https://github.com/Ultraschall/ultraschall-and-reaper-docs/blob/master/Docs/Reaper-ConfigVariables-Documentation.txt#L4371
 recModeNormalNoPreroll, recModeNormalWithPreroll, recModeTimeSelAutopunch, recModeAutopunchSelItems = 0, 1, 2, 3
 function GetRecMode()
-
   projrecmode = reaper.SNM_GetIntConfigVar("projrecmode", -666)
   if projrecmode == 1 then -- Record mode: normal
     prerollSettings = reaper.SNM_GetIntConfigVar("preroll", -666)
@@ -79,15 +79,23 @@ function GetRecMode()
   end
 end
 
+function DoAtExit()
+  -- set toggle state to off
+  reaper.SetToggleCommandState(sectionID, cmdID, 0);
+  reaper.RefreshToolbar2(sectionID, cmdID);
+end
 
+--------------------------------------
+-- Mute/Unmute HW outs
+--------------------------------------
 function WaitForPrerollAndMuteHWouts()
   playPos = reaper.GetPlayPosition()
   if playPos >= prerollStopPos then
     MuteHWOuts()
     return
   else 
-    reaper.defer(WaitForPrerollAndMuteHWouts)
     -- Msg("Defer WaitForPreroll")
+    reaper.defer(WaitForPrerollAndMuteHWouts)
   end
 end
 
@@ -118,10 +126,10 @@ end
 
 
 tracksWhoseHWoutsWereSetToMute_Table = {}
-tracksWhoseHWoutsWereSetToMute_Table_pos = 1
+tracksWhoseHWoutsWereSetToMute_Table_Idx = 1
 function MuteHWOuts()
-  tracksCount = reaper.CountTracks(0)
   
+  tracksCount = reaper.CountTracks(0)
   for i = 0, tracksCount-1  do
     HWoutWasSetToMute = false
     local tr =  reaper.GetTrack(0, i)
@@ -142,8 +150,8 @@ function MuteHWOuts()
     end -- if isRecArmed == 1 then
     
     if (HWoutWasSetToMute) then
-      tracksWhoseHWoutsWereSetToMute_Table[tracksWhoseHWoutsWereSetToMute_Table_pos] = tr
-      tracksWhoseHWoutsWereSetToMute_Table_pos = tracksWhoseHWoutsWereSetToMute_Table_pos + 1 
+      tracksWhoseHWoutsWereSetToMute_Table[tracksWhoseHWoutsWereSetToMute_Table_Idx] = tr
+      tracksWhoseHWoutsWereSetToMute_Table_Idx = tracksWhoseHWoutsWereSetToMute_Table_Idx + 1 
     end
 
   end -- loop through tracks
@@ -151,6 +159,7 @@ end -- MuteHWOuts()
 
 
 function UnmuteHWouts()
+
   for _, tr in ipairs(tracksWhoseHWoutsWereSetToMute_Table) do
     -- Msg("In table")
     if reaper.ValidatePtr(tr, "MediaTrack*") then
@@ -168,17 +177,70 @@ function UnmuteHWouts()
 
 end
 
-
-function DoAtExit()
-  -- set toggle state to off
-  reaper.SetToggleCommandState(sectionID, cmdID, 0);
-  reaper.RefreshToolbar2(sectionID, cmdID);
+--------------------------------------
+-- Disable/Enable Input monitoring   
+--------------------------------------
+function GetPrerollMeasures()
+  local prm = reaper.SNM_GetDoubleConfigVar("prerollmeas", -666)
+  -- Msg("prm: " .. prm)
+  return prm
 end
 
+function WaitForPrerollEndandReenableInputMonitoring()
+  playPos = reaper.GetPlayPosition()
+  if playPos >= prerollStopPos then
+    ReenableInputMonitoring()
+    return
+  else 
+    -- Msg("Defer WaitForPreroll")
+    reaper.defer(WaitForPrerollEndandReenableInputMonitoring)
+  end
+end
 
---------------
---- Main() ---
---------------
+tracksWhoseRecMonWereDisabled_Table = {}
+tracksWhoseRecMonWereDisabled_Table_Idx = 1
+recMonDisabledOnAtLeastOneTrack = false
+function DisableInputMonitoring()
+
+  tracksCount = reaper.CountTracks(0)
+  for i = 0, tracksCount-1  do
+    local tr =  reaper.GetTrack(0, i)
+    isRecArmed = reaper.GetMediaTrackInfo_Value(tr, "I_RECARM")
+    if isRecArmed == 1 then
+      -- Msg(isRecArmed)
+      recMonMode =  reaper.GetMediaTrackInfo_Value(tr, "I_RECMON")
+      -- Msg("Mon mode" .. recMonMode)
+      if recMonMode == 1 or recMonMode == 2 then -- normal or tapestyle
+        reaper.SetMediaTrackInfo_Value( tr, "I_RECMON", 0) -- off
+        recMonDisabledOnAtLeastOneTrack = true
+        
+        tracksWhoseRecMonWereDisabled_Table[tracksWhoseRecMonWereDisabled_Table_Idx] = {}
+        tracksWhoseRecMonWereDisabled_Table[tracksWhoseRecMonWereDisabled_Table_Idx].tr = tr
+        tracksWhoseRecMonWereDisabled_Table[tracksWhoseRecMonWereDisabled_Table_Idx].recMonMode = recMonMode
+        tracksWhoseRecMonWereDisabled_Table_Idx = tracksWhoseRecMonWereDisabled_Table_Idx + 1 
+      end
+    end -- if isRecArmed == 1 then  
+  end -- loop through tracks
+  if recMonDisabledOnAtLeastOneTrack then 
+    WaitForPrerollEndandReenableInputMonitoring()
+  end
+  
+end
+
+function ReenableInputMonitoring()
+  for i = 1, #tracksWhoseRecMonWereDisabled_Table do
+    -- Msg("In table")
+    tr = tracksWhoseRecMonWereDisabled_Table[i].tr
+    if reaper.ValidatePtr(tr, "MediaTrack*") then
+      -- Msg("Validated!")
+      reaper.SetMediaTrackInfo_Value(tr, "I_RECMON",  tracksWhoseRecMonWereDisabled_Table[i].recMonMode)
+    end
+  end
+end
+
+--------------------------------------
+-- Main()
+--------------------------------------
 lastIsRecording = -1
 function Main()     
   isRecording = reaper.GetToggleCommandState(1013) -- "Transport: Record", return values: 0 = not recording, 1 = recording
@@ -188,22 +250,33 @@ function Main()
     
     if isRecording == 1 then  
       recMode = GetRecMode()
+      prerollStopPos = reaper.GetCursorPosition()
       if recMode == recModeNormalNoPreroll then
         MuteHWOuts()
       elseif recMode == recModeNormalWithPreroll then
-        prerollStopPos = reaper.GetCursorPosition()
+
+        -- prerollStopPos = reaper.GetCursorPosition()
         WaitForPrerollAndMuteHWouts()
+
       elseif recMode == recModeTimeSelAutopunch then
+
         timeSelStart, timeSelEnd = reaper.GetSet_LoopTimeRange(false, false, 0, 0, false) -- get cur. time sel. values
         if timeSelEnd > timeSelStart then
           WaitForTimeSelStartAndMuteHWOuts()
         end
+
       elseif recMode == recModeAutopunchSelItems then
         -- TODO --
       end
+
+      if DISABLE_INPUT_MONITORING_DURING_PRE_ROLL and GetRecMode() ~= recModeNormalNoPreroll and GetPrerollMeasures() >= 1 then
+        DisableInputMonitoring()
+      end
+    
     else --  isRecording == 0, unmute the HWouts
-      Msg("Unmute HW outs called")
+      -- Msg("Unmute HW outs called")
       UnmuteHWouts()
+      
     end -- if isRecording
 
     lastIsRecording = isRecording
@@ -212,9 +285,10 @@ function Main()
   reaper.defer(Main)
 end -- Main()
 
--------------
---- start ---
--------------
+
+--------------------------------------
+-- start
+--------------------------------------
 -- set toggle state to on
 _, _, sectionID, cmdID, _, _, _ = reaper.get_action_context()
 reaper.SetToggleCommandState(sectionID, cmdID, 1);
