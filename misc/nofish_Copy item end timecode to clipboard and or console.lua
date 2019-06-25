9ee67c82892dec0113daba16d0aba5a0d70b846f
nofish <nofishonfriday@googlemail.com>
2019-06-26 00:07:02 +0200

    add script: nofish_Copy item end timecode to clipboard and or console.lua

---
+++ misc/nofish_Copy item end timecode to clipboard and or console.lua
@@ -0,0 +1,68 @@
+--[[
+ * ReaScript Name: Copy item end timecode to clipboard and or console.lua
+ * Version: 1.0
+ * Author: nofish
+ * Donation: https://paypal.me/nofish
+ * About:
+ *  Copies first sel. item's end timecode to cliboard and / or shows it in console (see script's USER CONFIG AREA)
+--[[
+ * Changelog:
+  
+ * v1.0 - June 25 2019
+    + initial release
+--]]
+
+
+-- USER CONFIG AREA -----------------------------------------------------------
+
+showInConsole   = true -- true/false: display item end timecode in console
+copyToClipboard = false -- true/false: auto copy item end timecode to clipboard (needs SWS)
+
+------------------------------------------------------- END OF USER CONFIG AREA
+
+
+function msg(m)
+  return reaper.ShowConsoleMsg(tostring(m) .. "\n")
+end
+
+function SecondsToTimecode(seconds)
+  local tc = ""
+  local isNegative = false
+  if seconds < 0 then
+    seconds = math.abs(seconds)
+    isNegative = true
+  end
+  fullsecs, secsfrac  = math.modf(seconds)
+  -- https://gist.github.com/jesseadams/791673
+  hours     = string.format("%01.f", math.floor(fullsecs/3600))
+  mins      = string.format("%01.f", math.floor(fullsecs/60 - (hours*60)))
+  secs      = string.format("%02.f", math.floor(fullsecs - hours*3600 - mins*60))
+  millisecs = string.format("%03.f", string.sub(secsfrac, 2, 5))
+  
+  if isNegative == true then
+    tc = tc .. "-"
+  end
+  if tonumber(hours) > 0 then
+    tc = tc .. hours .. ":".. mins .. ":" .. secs .. "." .. millisecs
+  else
+    tc = tc .. mins .. ":" .. secs .. "." .. millisecs
+  end
+  if showInConsole == true then
+    msg(tc)
+  end
+  return tc
+end
+
+function Main()
+  item =  reaper.GetSelectedMediaItem(0, 0)
+  if item ~= nil then
+    pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+    endpos = pos + reaper.GetMediaItemInfo_Value(item, "D_LENGTH") + reaper.GetProjectTimeOffset(0, 0)
+    endposTimecode = SecondsToTimecode(endpos)
+    if copyToClipboard == true then
+      reaper.CF_SetClipboard(endposTimecode)
+    end 
+  end
+end
+
+reaper.defer(Main)
